6e86e5f35f3ff83f36d51b62b8205f36c0c3f833
namdz121 <tennam2004@gmail.com>
2026-08-30 23:37:14 +0700

    lad3

---
+++ Lad_03/Lad_03/Lad_03/Controllers/NamAboutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lad_03.Controllers
+{
+    public class NamAboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.name = "Nguyễn Văn Nam";
+
+            ViewData["classes"] = "LTW2";
+            TempData["module"] = "lad 3";
+            return View();
+        }
+    }
+}
